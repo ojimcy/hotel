1d47fd2e6b5e4828dfd9b34742f732a870987a64
Ojimcy <ojimcy237@gmail.com>
2020-09-10 15:26:40 +0100

    Route created

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import { Route, Switch } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
-    </div>
+    <>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms' component={Rooms} />
+        <Route exact path='/rooms:slug' component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
